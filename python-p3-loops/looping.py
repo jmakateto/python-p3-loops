cf594fb261e2d751608ed4f2f6895254b5c281b5
john makateto <jmakateto@gmail.com>
2023-08-24 23:19:18 +0300

    Add user registration functionality

---
+++ python-p3-loops/looping.py
@@ -0,0 +1,19 @@
+def happy_new_year():
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+def square_integers(numbers):
+    squared_numbers = [num ** 2 for num in numbers]
+    return squared_numbers
+def fizzbuzz():
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
